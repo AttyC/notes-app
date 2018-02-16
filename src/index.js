23dc4ac3f6c3296600769106eb89525c372659d7
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-16 13:12:29 +0000

    single note view

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@
     // var HTMLelement = null;
     var noteList = new NoteList();
     noteList.createNote('Favourite drink: sweet cold Milo Ice');
+  var singleNoteView = new SingleNoteView(noteList.notes[0]);
     var noteView = new NoteListView(noteList);
     var noteController = new NoteController(noteView);
-    var singleNoteView = new SingleNoteView(notelist.note);
     noteController.insertHtml();
   }
   exports.init = init;
