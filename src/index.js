f5f4373f7890c9f27bec49eb9dd3c5a027c37966
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-18 14:27:39 +0000

    add form to collect notes

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@
   function init(){
 
     var noteList = new NoteList();
-    noteList.createNote('Favourite drink: sweet cold Milo Ice');
-    noteList.createNote('Blah test with some more words to check length');
-    noteList.createNote('Sweet test sweets sweetie, for fun');
     var singleNoteView = new SingleNoteView(noteList.notes[0]);
     var noteView = new NoteListView(noteList);
     var noteController = new NoteController(noteView);
     noteController.insertHtml();
-
-    window.addEventListener('hashchange', function () {
-      var id = location.hash.split("#notes/")[1];
-      document
-        .getElementById("show")
-        .innerHTML = noteList.notes[id].text;
-    }, false);
-
+    noteController.changeUrl(noteList);
+    noteController.formSubmit(noteController, noteList);
   }
   exports.init = init;
 })(this)
