55867718a74b2b160702c05a94f0da2534b871ee
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-18 14:28:59 +0000

    function for hash change and collect form data

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -8,6 +8,31 @@
   NoteController.prototype.insertHtml = function () {
     this.element.innerHTML = this.noteView.createHTML();
   }
+
+  NoteController.prototype.changeUrl = function(noteList){
+    window.addEventListener('hashchange', function () {
+      this.noteList = noteList;
+      var id = location.hash.split("#notes/")[1];
+      document
+        .getElementById("show")
+        .innerHTML = this.noteList.notes[id].text;
+    }, false);
+  }
+
+  NoteController.prototype.formSubmit = function(noteController, noteList){
+
+    document.getElementById('text-form').addEventListener('submit', function (event) {
+      this.noteController = noteController;
+      this.noteList = noteList;
+      event.preventDefault();
+      var textContent = document.getElementById("text").value;
+      document.getElementById("text").value = '';
+      this.noteList.createNote(textContent);
+      noteController.insertHtml();
+    }, false);
+
+  }
+
   exports.NoteController = NoteController;
 
 })(this)
